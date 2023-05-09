bc1a2d31c9ee90e915b40c6b55d745121da24512
Rohan-Shakya <rohanshakya254@gmail.com>
2023-05-09 21:57:32 +0545

    done new smart contract with unique id and slug field

--- smart-contracts/contracts/CryptoFunding.sol
+++ smart-contracts/contracts/CryptoFunding.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.9;
 
 contract CryptoFunding {
     struct Campaign {
+        bytes32 id;
         address owner;
         string title;
         string description;
@@ -10,16 +11,20 @@ contract CryptoFunding {
         uint256 deadline;
         uint256 amountCollected;
         string image;
+        string slug;
         address[] donators;
         uint256[] donations;
     }
 
     Campaign[] public campaigns;
 
-    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image, string memory _slug) public returns (bytes32) {
         require(_deadline > block.timestamp, "The deadline should be a date in the future.");
 
+        bytes32 id = keccak256(abi.encodePacked(block.timestamp, msg.sender, campaigns.length));
+
         Campaign memory newCampaign = Campaign({
+            id: id,
             owner: _owner,
             title: _title,
             description: _description,
@@ -27,16 +32,19 @@ contract CryptoFunding {
             deadline: _deadline,
             amountCollected: 0,
             image: _image,
+            slug: _slug,
             donators: new address[](0),
             donations: new uint256[](0)
         });
 
         campaigns.push(newCampaign);
 
-        return campaigns.length - 1;
+        return id;
     }
 
     function donateToCampaign(uint256 _id) public payable {
+        require(_id < campaigns.length, "The specified campaign ID does not exist.");
+
         Campaign storage campaign = campaigns[_id];
 
         campaign.donators.push(msg.sender);
@@ -49,13 +57,27 @@ contract CryptoFunding {
         campaign.amountCollected += msg.value;
     }
 
-    function getCampaignDonators(uint256 _id) public view returns (address[] memory, uint256[] memory) {
-        Campaign storage campaign = campaigns[_id];
+    function getCampaignDonators(bytes32 _id) public view returns (address[] memory, uint256[] memory) {
+        for (uint256 i = 0; i < campaigns.length; i++) {
+            if (campaigns[i].id == _id) {
+                return (campaigns[i].donators, campaigns[i].donations);
+            }
+        }
 
-        return (campaign.donators, campaign.donations);
+        revert("The specified campaign ID does not exist.");
     }
 
     function getAllCampaigns() public view returns (Campaign[] memory) {
         return campaigns;
+    }
+
+    function getCampaign(bytes32 _id) public view returns (Campaign memory) {
+        for (uint256 i = 0; i < campaigns.length; i++) {
+            if (campaigns[i].id == _id) {
+                return campaigns[i];
+            }
+        }
+
+        revert("The specified campaign ID does not exist.");
     }
 }
